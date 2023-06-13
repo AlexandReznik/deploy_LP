c5f216379f825d2fef9ab7ce22319c92f5724689
AlexandrReznik <saha.reznik2004@gmail.com>
2023-06-13 10:11:19 +0300

    Change settings

--- config/settings.py
+++ config/settings.py
@@ -27,8 +27,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = str(os.getenv('SECRET_KEY'))
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'RENDER' not in os.environ
 
+ALLOWED_HOSTS = ["*"]
+
+RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
+if RENDER_EXTERNAL_HOSTNAME:
+    ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
 
 # Application definition
 
@@ -188,7 +193,7 @@ LOGGING = {
 }
 
 
-ALLOWED_HOSTS = ["*"]
+
 if DEBUG:
     INTERNAL_IPS = [
         "192.168.1.4",
