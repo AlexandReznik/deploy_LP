138120fa367de8295fcedc66deb7a9582e4f2a44
AlexandrReznik <saha.reznik2004@gmail.com>
2023-06-14 13:08:40 +0300

    ///

--- config/settings.py
+++ config/settings.py
@@ -149,7 +149,7 @@ STATIC_URL = '/static/'
 if not DEBUG:
     
     # STATIC_ROOT = BASE_DIR / 'static'
-    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+    STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 # STATICFILES_DIRS = [
 #     BASE_DIR / 'static',
 # ]
