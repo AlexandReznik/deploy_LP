c671f84ea10c72fdab1dcdbf143654bc903f3d58
AlexandrReznik <saha.reznik2004@gmail.com>
2023-06-14 13:23:19 +0300

    add static

--- config/settings.py
+++ config/settings.py
@@ -147,12 +147,16 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
-    BASE_DIR / 'static',
+    BASE_DIR / 'static/css/',
+    BASE_DIR / 'static/img/',
+    BASE_DIR / 'static/js/',
+    BASE_DIR / 'static/webfonts/',
+    BASE_DIR / 'static/favicon.ico',
 ]
 if not DEBUG:
     
     # STATIC_ROOT = BASE_DIR / 'static'
-    STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
     STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
