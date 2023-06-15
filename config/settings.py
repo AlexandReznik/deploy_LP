9947e23da51c1cd264e88a128023ee8360933e02
AlexandrReznik <saha.reznik2004@gmail.com>
2023-06-15 12:52:31 +0300

    static

--- config/settings.py
+++ config/settings.py
@@ -158,7 +158,7 @@ STATIC_ROOT = os.path.join(BASE_DIR , 'staticfiles')
     # STATIC_ROOT = BASE_DIR / 'static/'
 # STATIC_ROOT = BASE_DIR / 'staticfiles'
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
