c9c74c937481c6a7d72a014e4bab94b8d70186e3
AlexandrReznik <saha.reznik2004@gmail.com>
2023-07-17 17:00:19 +0300

    User can send messages to support

--- mainapp/views.py
+++ mainapp/views.py
@@ -250,31 +250,4 @@ class DocSitePageView(TemplateView):
 class ContactsView(TemplateView):
     template_name = 'mainapp/contacts.html'
 
-    def get_context_data(self, **kwargs):
-        context = super(ContactsView, self).get_context_data(**kwargs)
-        if self.request.user.is_authenticated:
-            context["form"] = mainapp_forms.MailFeedbackForm(
-                user=self.request.user
-            )
-        return context
-
-    def post(self, *args, **kwargs):
-        if self.request.user.is_authenticated:
-            cache_lock_flag = cache.get(
-                f"mail_feedback_lock_{self.request.user.pk}"
-            )
-            if not cache_lock_flag:
-                cache.set(
-                    f"mail_feedback_lock_{self.request.user.pk}", "lock", timeout=300,)
-                messages.add_message(
-                    self.request, messages.INFO, _("Message sended"))
-                mainapp_tasks.send_feedback_mail.delay({
-                    "user_id": self.request.POST.get("user_id"),
-                    "message": self.request.POST.get("message"),
-                }
-                )
-            else:
-                messages.add_message(
-                    self.request, messages.WARNING, ("You can send only one message per 5 minutes"),)
-        return HttpResponseRedirect(reverse_lazy("mainapp:contacts"))
-
+    
