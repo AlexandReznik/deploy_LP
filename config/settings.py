592a10fba31ac82f28468d5478d1f7d41e1f3236
AlexandrReznik <saha.reznik2004@gmail.com>
2023-10-02 21:30:15 +0200

    .../

--- config/settings.py
+++ config/settings.py
@@ -160,7 +160,8 @@ USE_TZ = True
 STATIC_URL = '/staticfiles/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
-    os.path.join(BASE_DIR, 'media'),
+    # os.path.join(BASE_DIR, 'media'),
+    os.path.join(BASE_DIR, 'uploads'),
 ]
 STATIC_ROOT = os.path.join(BASE_DIR , 'staticfiles')
     
