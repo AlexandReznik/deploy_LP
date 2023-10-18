71eca2d543f8e6655fbe1f349367ecfb58749bf5
AlexandReznik <saha.reznik2004@gmail.com>
2023-10-18 12:00:21 +0200

    ,,..

--- config/settings.py
+++ config/settings.py
@@ -108,18 +108,18 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.postgresql_psycopg2',
-    #     'NAME': 'lplearning',
-    #     'USER': 'admin',
-    #     'PASSWORD': 'Kinzer2004',
-    #     'HOST': 'localhost',
-    #     'PORT': ''
-    # }
-    'default': dj_database_url.config(
-        default=str(os.getenv('default')),
-        conn_max_age=600
-    )
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'lplearning',
+        'USER': 'admin',
+        'PASSWORD': 'Kinzer2004',
+        'HOST': 'localhost',
+        'PORT': ''
+    }
+    # 'default': dj_database_url.config(
+    #     default=str(os.getenv('default')),
+    #     conn_max_age=600
+    # )
 }
 
 
